8508023529a59ca9f3a8e8ab29db474333cbb098
helena furtado <helenafurtado25@gmail.com>
2026-02-09 15:55:04 -0300

    adicionando personagem 1

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pygame
 
 pygame.init()
 
-tela = pygame.display.set_mode((800, 400)) #criando a janela de exibicao do jogo 
+janela = pygame.display.set_mode((960, 540)) #criando a janela de exibicao do jogo 
  
-pygame.display.set_caption('Guerra na galáxia) # criando um nome para a janela 
+pygame.display.set_caption("Guerra na galáxia") # criando um nome para a janela 
 
 imagem_fundo = pygame.image.load("C:/Users/helen/OneDrive/Desktop/jogo python/imagens/img_galaxia.jpg")
+
+nave_jogador = pygame.image.load("C:/Users/helen/OneDrive/Desktop/jogo python/imagens/nave_jogador.png")
 
 # problema 1 -> a janela de exibicao abre, mas fecha imediatamente. PARA RESOLVER: (criar um loop para rodar sempre o codigo)
 
@@ -20,6 +22,8 @@ while loop: # mantem a janela do jogo aberta
           if events.type == pygame.QUIT:
             loop = False
 
+    janela.blit(imagem_fundo, (0, 0)) # mostra a imagem de fundo
+    janela.blit(nave_jogador, (470, 200))
 
 
 
